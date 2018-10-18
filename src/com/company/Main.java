938618c65652a8aaa19316b3868c4c774f8745c1
raulrp3 <raul.ramirez@escuelaestech.es>
2018-10-18 17:10:13 +0200

    Ejercicio 16

---
+++ src/com/company/Main.java
@@ -0,0 +1,66 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+    public static int validarEntero(Scanner sc) {
+        int numeroValidado;
+        while (!sc.hasNextInt()) {
+            System.out.print("¡Introduce un valor numérico entero! ");
+            sc.next();
+        }
+        numeroValidado = sc.nextInt();
+        return numeroValidado;
+    }
+
+    public static int daDias(int mes) {
+        int numeroDias = 0;
+        switch (mes) {
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                numeroDias = 30;
+                break;
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                numeroDias = 31;
+                break;
+            case 2:
+                numeroDias = 28;
+        }
+        return numeroDias;
+    }
+
+    public static boolean validarFecha(int dia, int mes, int año, int numeroDias) {
+        boolean validado = true;
+        if ((dia < 0 || dia > numeroDias) || (mes < 0 || mes > 12) || año < 0) {
+            validado = false;
+        }
+        return validado;
+    }
+
+    public static void main(String[] args) {
+        int dia, mes, año, numeroDias;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("¡Vamos a indicar si una fecha es correcta!");
+        System.out.print("Introduce un día: ");
+        dia = validarEntero(sc);
+        System.out.print("Introduce un mes: ");
+        mes = validarEntero(sc);
+        System.out.print("Introduce un año: ");
+        año = validarEntero(sc);
+        sc.close();
+        numeroDias = daDias(mes);
+        if (validarFecha(dia, mes, año, numeroDias)) {
+            System.out.printf("La fecha %d/%d/%d es: CORRECTA\n", dia, mes, año);
+        } else {
+            System.out.printf("La fecha %d/%d/%d es: INCORRECTA\n", dia, mes, año);
+        }
+    }
+}
